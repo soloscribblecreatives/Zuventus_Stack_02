dd689208318ca0851c015f261d11bd9c989e3403
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2026-06-26 11:51:11 +0530

    Update

--- global.js
+++ global.js
@@ -198,7 +198,7 @@ if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.ge
 	
  } 
  
-	if(nextSlideNo <= 8){//number 3 is number of total slides present
+	if(nextSlideNo <= 9){//number 3 is number of total slides present
 	// alert(nextSlideNo);
 	var tempNext = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+nextSlideNo);
 
@@ -302,10 +302,10 @@ if(direction == 'b') {
 //custom slide changes ends here....
 
 	else{
-	if(page_id <= 8){
+	if(page_id <= 9){
 		page_id = page_id + 1;
 		//alert(page_id);
-		if(page_id == 9){
+		if(page_id == 10){
             flag=1;
         }
 	}
@@ -407,6 +407,9 @@ switch(pg_id){
 	break;
 	case 8:
 	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><iframe src="8/html/index.html" class="background1" height="775" width="1180" frameBorder="0" title="Iframe Example"></iframe>';
+	break;
+	case 9:
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><iframe src="9/html/index.html" class="background1" height="775" width="1180" frameBorder="0" title="Iframe Example"></iframe>';
 	break;
 }
 
